e6d7d6adbf978b0c6c1eb2ff9b4105c0286be918
xdh <247105782@qq.com>
2020-03-24 20:34:59 +0800

    登录页面的准备工作
    Signed-off-by: xdh <247105782@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+		path: "/login",
+		// 除了首页之外的页面都可以用懒加载,@符号代表的src这个目录
+		// .vue这个扩展名可以忽略
+		component: () => import("@/views/Login")
+	}
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>登录页
+        <span class="iconfont iconnew"></span>
+        <span class="iconfont iconweixin"></span>
+    </div>
+</template>
+<script>
+export default {
+    
+};
+</script>
+<style>
+.iconnew {
+    font-size: 50px;
+    color: orange;    
+}
+.iconweixin {
+    font-size: 50px;
+    color: green;
+}
+</style>
