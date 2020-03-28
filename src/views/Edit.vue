cf46f8cfa7b264c80c37276d60fc208ee84a9855
xdh <247105782@qq.com>
2020-03-28 22:17:29 +0800

    图片上传的渲染到页面，封装修改用户信息的axios，上传完之后调用，修改用户头像

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -26,7 +26,9 @@ export default {
     data(){
         return {
             // 用户详情
-            userInfo:{}
+            userInfo:{},
+            // 本地的用户数据
+            userJson: {},
         }
     },
     components: {
@@ -37,6 +39,10 @@ export default {
         // 从本地存储里面拿到对象  对象里面有id
         const userJson = JSON.parse(localStorage.getItem('userInfo'))
         //请求用户详情
+        // console.log(userJson);
+        // 保存到data，就可以在methods的方法是调用了
+         this.userJson = userJson;
+
         this.$axios({
             url:"/user/" + userJson.user.id,
             headers : {
@@ -51,8 +57,52 @@ export default {
     methods:{
         // 图片上传方法
         afterRead(file){
-            console.log(file);
+            //创建一个表单对象，上传图片资源 必须是表单形式，不能是json
+            const fd = new FormData();
+            //append 给表单添加元素， 
+            //第一个字符串file表示接口属性，第二个是我们获取的对象
+            fd.append("file",file.file)
+            // console.log(file);
+            this.$axios({
+                method:"post",
+                url:"/upload",
+                headers : {
+                Authorization:this.userJson.token
+                },
+                data:fd
+            }).then(res=>{
+                // console.log(res);
+                // 解构出来的url就是图片路径
+                const {url} = res.data.data;
+                // 因为上面的img  src 里面已经加了基地址，所以直接赋值
+                this.userInfo.head_img = url;
+                // 单纯了上传一张图片 没有修改用户的头像信息，
+                // 还要去编辑用户的axios里面去处理
+                // 所以封装一个函数方法，上传了就调用方法，修改到后台
+                this.handleEdit({
+                    head_img:url
+                })
+
+            })
+        },
+
+        //编辑用户信息的函数
+        handleEdit(data){
+            this.$axios({
+                // 获取用户详情的时候，已经把用户信息保存在userInfo里面了
+                url:"/user_update/" + this.userInfo.id,
+                method:"post",
+                headers : {
+                Authorization:this.userJson.token
+                },
+                data:data,
+            }).then(res=>{
+                // console.log(res);
+                
+            })
         }
+
+
     }
 
 }
