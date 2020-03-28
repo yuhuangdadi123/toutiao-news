2ed6376e8536972f713249dc45c9da09aa4c96da
xdh <247105782@qq.com>
2020-03-28 19:03:00 +0800

    调用组件 完成编辑页的静态布局

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-      <!-- 头部导航组件 -->
-      <NavigateBar title="编辑资料" />
+
+    <!-- 头部导航组件 -->
+    <NavigateBar title="编辑资料" />
+    <!-- 头像 -->
+    <div class="avatar">
+        <img src="../assets/logo.png" alt="">
+    </div>
+    <!-- 按钮列表 -->
+    <Listbar label="昵称" tips="火星网友" />
 
   </div>
 </template>
@@ -19,6 +26,33 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.avatar{
+    display: flex;
+    padding: .555556rem;
+    justify-content: center;
+    align-items: center;
+    img{
+        width: 2.777778rem;
+        height: 2.777778rem;
+        border-radius: 50%;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </style>
