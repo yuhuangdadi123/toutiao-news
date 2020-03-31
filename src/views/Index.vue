d02fe66c2f5fb2c3ce3516030df6b3ce1cdb0eb4
xdh <247105782@qq.com>
2020-03-31 23:52:30 +0800

    封装首页的文章组件

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -15,16 +15,60 @@
         </router-link>
    </div>
 
+    <!-- tab栏 -->
+    <div class="tab">
+        <!-- v-model：就是当前的索引值，是唯一的，比较类似于for循环的key -->
+        <!-- sticky：是否使用粘性定位布局 -->
+        <!-- swipeable: 是否开启手势滑动切换 -->
+        <van-tabs v-model="active" sticky swipeable>
+        <van-tab v-for="(item, index) in categories" :title="item" :key="index">
+        
+         <!-- 假设list是后台返回的数组，里有10个元素 -->
+                    <div v-for="(item, index) in list" :key="index">
+                        <!-- 只有单张图片的 -->
+                        <PostItem1/> 
+                    </div>
 
-
-
+        </van-tab>
+        </van-tabs>
+   
+    </div>
 
 </div>
 </template>
 
 <script>
-export default {
+// 文章列表的组件,只有单张图片的
+import PostItem1 from "@/components/PostItem1"
+// 大于等于3张图片的组件
+import PostItem2 from "@/components/PostItem2"
+// 视频的列表组件
+import PostItem3 from "@/components/PostItem3"
 
+export default {
+    data(){
+        return{
+            categories:['关注','娱乐','体育','汽车','房产',
+            '关注','娱乐','体育','汽车','房产','关注','∨'],
+            active: 0,
+            list: [1,1,1,1,1,1,1,1,1,1], // 10个1
+        }
+    },
+    // 监听属性
+    watch: {
+        // 监听tab栏的切换
+        active(){
+            // 判断如果点击的是最后一个图标，跳转到栏目管理页
+            if(this.active === this.categories.length - 1){
+                this.$router.push("/栏目管理")
+            }
+        }
+    },
+    components: {
+        PostItem1,
+        PostItem2,
+        PostItem3
+    },
 }
 </script>
 
@@ -69,16 +113,36 @@ export default {
 }
 
 
+/deep/ .van-tabs__nav {
+    background-color: #ebebea;
+     position: static;
+}
 
+/deep/ .van-tab:nth-last-child(2){
+    width: 1.111111rem!important;
+    position: absolute;
+    right:0; 
+    top: 0;
+    font-size: .694444rem;
+    box-sizing:unset;
+    background-color: #ebebea;
+}
 
+/deep/ .van-tabs__line{
+    display: none;
+}
 
+/deep/ .van-tab--active{
+    border-bottom: 3px #ff0000 solid;
+}
 
+/deep/ .van-tab{
+    flex-basis: 20%!important;
+}
 
-
-
-
-
-
+/deep/ .van-tabs__wrap{
+    padding-right: 1.388889rem;
+}
 
 
 </style>
