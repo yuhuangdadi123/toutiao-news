1fc0bb5b3c304aebd36818949ab844128d64c78e
xdh <247105782@qq.com>
2020-04-02 20:23:19 +0800

    请求渲染栏目的数据，并请求头条栏目的文章

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -21,7 +21,7 @@
         <!-- sticky：是否使用粘性定位布局 -->
         <!-- swipeable: 是否开启手势滑动切换 -->
         <van-tabs v-model="active" sticky swipeable>
-        <van-tab v-for="(item, index) in categories" :title="item" :key="index">
+        <van-tab v-for="(item, index) in categories" :title="item.name" :key="index">
         
              <!-- 下拉刷新 -->
             <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
@@ -36,7 +36,19 @@
                     <!-- 假设list是后台返回的数组，里有10个元素 -->
                     <div v-for="(item, index) in list" :key="index">
                         <!-- 只有单张图片的 -->
-                        <PostItem1/> 
+                        <PostItem1 
+                        v-if="item.type === 1 && item.cover.length > 0 && item.cover.length < 3"
+                        :data="item"/>
+
+                        <!-- 大于等于3张图片 -->
+                        <PostItem2 
+                        v-if="item.type === 1 && item.cover.length >= 3"
+                        :data="item"/>
+
+                        <!-- 视频 -->
+                        <PostItem3 
+                        v-if="item.type === 2"
+                        :data="item"/> 
                     </div>
                 </van-list>
             </van-pull-refresh>
@@ -59,10 +71,11 @@ import PostItem3 from "@/components/PostItem3"
 export default {
     data(){
         return{
-            categories:['关注','娱乐','体育','汽车','房产',
-            '关注','娱乐','体育','汽车','房产','关注','∨'],
+            // categories:['关注','娱乐','体育','汽车','房产',
+            // '关注','娱乐','体育','汽车','房产','关注','∨'],
+            categories: [],
             active: 0,
-            list: [1,1,1,1,1,1,1,1,1,1], // 10个1
+            list: [], 
             loading: false, // 是否正在加载中
             finished: false, // 是否已经加载完毕
             refreshing: false , // 是否正在下拉加载
@@ -83,7 +96,85 @@ export default {
         PostItem2,
         PostItem3
     },
+
+    mounted(){
+        // 在请求之前，应该先判断本地有没栏目数据
+        const categories = JSON.parse(localStorage.getItem("categories"));
+        // 本地的token
+        const {token} = JSON.parse(localStorage.getItem('userInfo')) || {};
+        // 如果本地有数据，获取本地的数据来渲染
+        if(categories){
+            // 登录了(有token)但是第一条不是关注
+            if(categories[0].name !== "关注" && token){
+                // 获取栏目数据
+                this.getCategories(token);
+                return;
+            }
+            // 登录了(有token)但是第一条不是关注
+            if(categories[0].name === "关注" && !token){
+                // 获取栏目数据
+                this.getCategories();
+                return;
+            }
+            this.categories = categories;
+        }else{
+            // 没有本地的数据才去请求栏目接口
+            this.$axios({
+                url:"/category"
+            }).then(res=>{
+                // console.log(res);
+                // data就是数组
+                const {data} = res.data;
+                data.push({
+                    name: "∨"
+                })
+                this.categories = data;
+                // 把菜单的数据保存到本地
+                localStorage.setItem("categories", JSON.stringify(data));
+            })
+        }
+
+        // 请求文章列表，页面一开始都是请求头条栏目下的文章，头条栏目的id是999
+        this.$axios({
+            url: "/post",
+            // params就是url问号后面的参数
+            params: {
+                category: 999
+            }
+        }).then(res => {
+            // 文章的数据
+            const {data} = res.data;
+            // 保存到data的list中
+            this.list = data;
+        })
+
+
+    },
+
     methods:{
+        // 获取栏目数据, 如果有token加上到头信息。没有就不加
+         getCategories(token){
+            const config = { url: "/category" }
+            // 如果有token，把token添加到头信息中
+            // 如果有token请求回来的数据就会有关注的栏目，因为代表是登录的
+            // 如果没有token就没有关注的栏目
+            if(token){
+                config.headers = { Authorization: token };
+            }
+            // 没有本地的数据才去请求栏目接口
+            this.$axios(config).then(res => {
+                // 菜单的数据
+                const {data} = res.data;
+                // 给data添加一个点击跳转到栏目管理的图标
+                data.push({
+                    name: "∨"
+                })
+                this.categories = data;
+                // 把菜单的数据保存到本地
+                localStorage.setItem("categories", JSON.stringify(data));
+            })
+         },
+
         onLoad() {
         // 异步更新数据
         // setTimeout 仅做示例，真实场景中一般为 ajax 请求
