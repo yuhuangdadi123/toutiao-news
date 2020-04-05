d1087370438e6252355bba29bce58765229f1576
xdh <247105782@qq.com>
2020-04-05 16:08:15 +0800

    限制首页文章列表请求的次数

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ Vue.prototype.$axios = axios;
 
 // 给axios添加基准路径，添加完了之后请求的url就会拼接这个地址
 axios.defaults.baseURL = "http://localhost:3000"
+// axios.defaults.baseURL = "http://hmtoutiao-api.atlansic.com"
 
 // 注册vant插件 （vue.use都是用来注册插件）
 Vue.use(Vant);
